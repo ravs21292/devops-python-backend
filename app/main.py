bc7ba178862b7ef042f8b034d36258835e89b0ea
ravinder <sravinder591@gmail.com>
2026-03-30 18:19:37 -0400

    ressolve issue with test case for items

--- app/main.py
+++ app/main.py
@@ -1,10 +1,8 @@
 from fastapi import FastAPI
-
 from app.routes.health import router as health_router
 from app.routes.items import router as items_router
 
-app = FastAPI() 
+app = FastAPI()
 
 app.include_router(health_router)
-
-app.include_router(health_router)
+app.include_router(items_router)
